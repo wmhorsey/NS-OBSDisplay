0ed8df92d44a45c65697323e142702559e531dba
William M Horsey <wmhorsey@gmail.com>
2020-02-23 13:08:56 -0500

    Fixed some stuff
    and broke some other stuff.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
 var myDiv = document.getElementById('sl');
+var slide = [];
 var slideShow = [];
 var currentSlide = 0;
 
 function btnPrev() {
     if(currentSlide > 0) currentSlide--;
+    buildSlide();
+}
+
+//redo sizing, something doesn't look right.
+function btnRefresh() {
     resizeSlide();
 }
 
 function btnNext() {
     if(currentSlide < (slideShow.length - 1) ) currentSlide++;
+    buildSlide();
+}
+
+// puts together the contents of the slide  **  needs more features
+function buildSlide() {
+    slide = slideShow[currentSlide]
+    // send slide to sizing before it is complete
     resizeSlide();
 }
 
+// determines how to best display the slide  **  needs working features!  lol
 function resizeSlide() {
 
-    //update  to current slide
-    myDiv.innerHTML = slideShow[currentSlide];
+    // reset all screen element(s) to default
+    myDiv.style.left = 0;
+    myDiv.style.width = "100%";
+    var slideHeight = 0;
+    var slideLength = 0;
 
-    myDiv.innerHTML.trim();
+    //update div to current slide, make sure everything is aligning correctly
+    var countHolder = myDiv.innerHTML = slide.toString();
+    
+    countHolder = countHolder.split('</div>');
+    countHolder = countHolder[0].toString();
 
-    // reset screen element(s) to 0
-    myDiv.style.left = "0px";
-    myDiv.style.maxWidth = "";
+    for( var i = 0, counting = true; i < countHolder.length; i++){
+        if( countHolder.charAt(i) == "<") counting = false;
+        if( countHolder.charAt(i) == ">") counting = true;
+        if( counting == true ) slideLength++;
+    };
 
-    //how long is the text that goes on the slide...
-    var slideLength = slideShow[currentSlide].length;
-
-    var slideHeight = myDiv.clientHeight;
-    console.log( "slideHeight = " + slideHeight );
+    //slideHeight = myDiv.clientHeight;
+    //console.log( `myDiv.clientHeight = ${slideHeight}px` );
 
     // hide slide if no data.
     if (slideLength > 0) myDiv.style.display = "initial";
@@ -45,28 +65,36 @@ function resizeSlide() {
     else if(slideLength > 839) fSize = 16;
 
     // this didn't work as intended, but is going to be the basis of the self adjusting solution.
-    //font size = text size - the length of the slide / characters per line
-    //var fSize = tSize - Math.floor(slideLength / cPL)
+    // font size = text size - the length of the slide / characters per line
+    // var fSize = tSize - Math.floor(slideLength / cPL)
     
     // set font size based
     myDiv.style.fontSize = `${fSize}px`;
 
-    myDiv.innerHTML = slideShow[currentSlide];
-
-    if( slideHeight != myDiv.clientHeight ) {
-        // okay, so our height changed because of a wrap.  starting height / pixel height should get us a roughly
-        // the number of lines being used.  Then dividing the text evenly that many lines should justify the text
-        // better while still maximizing utilized screen space.
-        // slideHeight = myDiv.offsetHeight - slideHeight;
-        var lines = (myDiv.clientHeight - (fSize * .8 + 10) ) / fSize;
-        lines = Math.ceil(lines);
-
-        myDiv.style.maxWidth = `${(slideLength / lines) - 20 }ch`;
-    }
-
-    //adjust left edge to center on screen
-    myDiv.style.left = ( `${((window.innerWidth - myDiv.offsetWidth)/2)}px` )
+    if( document.getElementById('shrinkSlideCheckbox').checked == true) shrinkSlide();
+    if( document.getElementById('centerSlideCheckbox').checked == true) centerSlide();    
 }
+
+//horizontal shrink
+function shrinkSlide(){
+
+    myHeight = myDiv.clientHeight;
+    myWidth = myDiv.clientWidth;
+
+    do{ 
+        myWidth=myWidth - 5;
+        myDiv.style.width = `${myWidth}px`;
+    } while (myHeight == myDiv.clientHeight);
+
+    myDiv.style.width = `${myWidth + 5}px`
+}
+
+//adjust left edge to center on screen
+function centerSlide(){
+    myDiv.style.left = `${(window.innerWidth - myDiv.clientWidth) / 2}px`;
+}
+
+
 
 var openFile = function(event) {
     const input = event.target.files[0];
@@ -75,19 +103,14 @@ var openFile = function(event) {
 
     reader.onload = (event) => {
         const file = event.target.result;
-        const allLines = file.split("<end-slide />");
-        
-        // Reading line by line
-        allLines.forEach((line) => {
-            slideShow.push(line);
-            console.log(line);
-        });
+        slideShow = file.split("<end-slide />");
 
         document.getElementById('fileSelect').disabled = true;
         document.getElementById('prevBtn').disabled = false;
+        document.getElementById('refreshBtn').disabled = false;
         document.getElementById('nextBtn').disabled = false;
 
-        resizeSlide();
+        buildSlide();
     };
 
     reader.onerror = (event) => {
