ef716fbf49e0207ec5b3a00db206433f2888836b
William M Horsey <wmhorsey@gmail.com>
2020-02-28 11:01:53 -0500

    many changed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var myDiv = document.getElementById('sl');
+myDiv.style.maxWidth = `${window.innerWidth - 12}px`;
 var slide = [];
 var slideShow = [];
 var currentSlide = 0;
@@ -13,63 +14,85 @@ function btnRefresh() {
     resizeSlide();
 }
 
+function btnClearAll(){
+    delete slideShow;
+    delete slide;
+    currentSlide = 0;
+
+    toggleControls();
+    buildSlide();
+}
+
 function btnNext() {
-    if(currentSlide < (slideShow.length - 1) ) currentSlide++;
+    if(currentSlide < (slideShow.length - 2) ) currentSlide++;
     buildSlide();
 }
 
 // puts together the contents of the slide  **  needs more features
 function buildSlide() {
-    slide = slideShow[currentSlide]
+    slide = slideShow[currentSlide];
+    slide.text = slide.split('</span>', 1).filter(x => x);
+    
+    slide.text = slide[0];
+    if( slide[1] ) slide.tag = slide[1];
+    else slide.tag = "";
+    slide.output = slide.tag + '<br>' + slide.text;
+
+    console.log(slide, '|', slide.tag, '|', slide.text, '|', slide.output);
     // send slide to sizing before it is complete
     resizeSlide();
+
+    //if( myDiv.clientHeight < 60 ) slide[0]
+
+}
+
+// try to count only VISIBLE characters appearing in HTML formatted strings
+function htmlTextLength( countMe ){
+
+    var slideLength = 0;
+    for (index in countMe){
+        for( var i = 0, counting = true; i < countMe[index].length; i++){
+            if( countMe[index].charCodeAt(i) == 60 ) counting = false;           // disable counting while inside a tag... (or anything else in <>)
+            if( countMe[index].charCodeAt(i) == 62 ) {i++; counting = true;}     // reenable counting after a tag... (or anything else in <>)
+            if( countMe[index].charCodeAt(i) == 13 ||                            // disable counting for 'tab'
+                countMe[index].charCodeAt(i) == 10 ) continue;                   // disable counting for 'return'
+            
+            if( counting == true ) slideLength++;                                // Count it up!
+        };
+    };
+    return slideLength;
 }
 
 // determines how to best display the slide  **  needs working features!  lol
-function resizeSlide() {
+// tSize = text size, is the
+function resizeSlide( maxFontSize = 30, charsPerLine = 90) {
 
     // reset all screen element(s) to default
     myDiv.style.left = 0;
-    myDiv.style.width = "100%";
-    var slideHeight = 0;
+    myDiv.style.width = `100%`;
     var slideLength = 0;
 
     //update div to current slide, make sure everything is aligning correctly
-    var countHolder = myDiv.innerHTML = slide.toString();
+    var countHolder = myDiv.innerHTML = slide.output; //.toString();
     
-    countHolder = countHolder.split('</div>');
-    countHolder = countHolder[0].toString();
-
-    for( var i = 0, counting = true; i < countHolder.length; i++){
-        if( countHolder.charAt(i) == "<") counting = false;
-        if( countHolder.charAt(i) == ">") counting = true;
-        if( counting == true ) slideLength++;
-    };
-
-    //slideHeight = myDiv.clientHeight;
-    //console.log( `myDiv.clientHeight = ${slideHeight}px` );
-
+    countHolder = countHolder.split('</span>').filter(x => x);
+    slideLength = htmlTextLength( countHolder );
+    
     // hide slide if no data.
     if (slideLength > 0) myDiv.style.display = "initial";
     else myDiv.style.display = "none";
 
-    // I am working on a more elegant solution for this, but for now this works.
-    // these breakpoints are intended for a 1080p screen.
-    if(slideLength < 280) fSize = 30;
-    else if(slideLength > 279 && slideLength < 320 ) fSize = 28;
-    else if(slideLength > 319 && slideLength < 400 ) fSize = 26;
-    else if(slideLength > 399 && slideLength < 450 ) fSize = 24;
-    else if(slideLength > 449 && slideLength < 600 ) fSize = 22;
-    else if(slideLength > 599 && slideLength < 660 ) fSize = 20;
-    else if(slideLength > 659 && slideLength < 840 ) fSize = 18;
-    else if(slideLength > 839) fSize = 16;
 
-    // this didn't work as intended, but is going to be the basis of the self adjusting solution.
-    // font size = text size - the length of the slide / characters per line
-    // var fSize = tSize - Math.floor(slideLength / cPL)
-    
+    var fontSize = maxFontSize - (Math.floor(slideLength / charsPerLine) * 2);
+    console.log( `fSize: ${fontSize} = maxFontSize: ${maxFontSize} - Math.floor(slideLength: ${slideLength}/ charsPerLline: ${charsPerLine})` );
+
     // set font size based
-    myDiv.style.fontSize = `${fSize}px`;
+    myDiv.style.fontSize = `${fontSize}px`;
+
+    // if still to big, resize again...
+    console.log( `${myDiv.clientHeight} : ${myDiv.scrollHeight}`);
+
+    if( myDiv.clientHeight < myDiv.scrollHeight ) resizeSlide( fontSize, 90 );
 
     if( document.getElementById('shrinkSlideCheckbox').checked == true) shrinkSlide();
     if( document.getElementById('centerSlideCheckbox').checked == true) centerSlide();    
@@ -79,22 +102,34 @@ function resizeSlide() {
 function shrinkSlide(){
 
     myHeight = myDiv.clientHeight;
-    myWidth = myDiv.clientWidth;
+    myWidth = startWidth = myDiv.clientWidth;
 
-    do{ 
-        myWidth=myWidth - 5;
+    do{ myWidth = myWidth - 5;
         myDiv.style.width = `${myWidth}px`;
-    } while (myHeight == myDiv.clientHeight);
+    } while (myHeight == myDiv.clientHeight && myWidth > 0);
 
-    myDiv.style.width = `${myWidth + 5}px`
+    myDiv.style.width = `${myWidth + 15}px`;
+
+    if(myWidth < 2) myDiv.style.width = `${startWidth}px`;
 }
 
-//adjust left edge to center on screen
+//adjust left edge to center slide in the window, parseInt cleans up any 1/2 pixel sizes.
 function centerSlide(){
-    myDiv.style.left = `${(window.innerWidth - myDiv.clientWidth) / 2}px`;
+    myDiv.style.left = `${ parseInt(((window.innerWidth) - myDiv.clientWidth) / 2 - 1)}px`;
 }
 
+window.onresize = function(){
 
+    myDiv.style.maxWidth = `${window.innerWidth - 12}px`;
+}
+
+function toggleControls(){
+    document.getElementById('fileSelect').toggleAttribute('disabled');
+    document.getElementById('prevBtn').toggleAttribute('disabled');
+    document.getElementById('refreshBtn').toggleAttribute('disabled');
+    document.getElementById('nextBtn').toggleAttribute('disabled');
+    document.getElementById('clearAllBtn').toggleAttribute('disabled');
+}
 
 var openFile = function(event) {
     const input = event.target.files[0];
@@ -105,11 +140,7 @@ var openFile = function(event) {
         const file = event.target.result;
         slideShow = file.split("<end-slide />");
 
-        document.getElementById('fileSelect').disabled = true;
-        document.getElementById('prevBtn').disabled = false;
-        document.getElementById('refreshBtn').disabled = false;
-        document.getElementById('nextBtn').disabled = false;
-
+        toggleControls();
         buildSlide();
     };
 
